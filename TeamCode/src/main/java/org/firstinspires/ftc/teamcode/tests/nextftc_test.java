3804057dd8e9754a40dbdef3bf77a78f1b1a7410
Allen <allenliu@outlook.com>
2025-10-06 17:10:36 -0500

    fixed the error, i was using 0.6 instead of 1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/nextftc_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/nextftc_test.java
@@ -1,18 +1,17 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import static com.rowanmcalpin.nextftc.ftc.OpModeData.telemetry;
-
 import androidx.annotation.NonNull;
-
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.rowanmcalpin.nextftc.ftc.hardware.controllables.MotorEx;
-
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.ftc.components.BulkReadComponent;
+import dev.nextftc.hardware.driving.*;
 import org.apache.commons.math3.stat.descriptive.moment.SemiVariance;
-
 import dev.nextftc.core.commands.Command;
 import dev.nextftc.ftc.Gamepads;
 import dev.nextftc.ftc.NextFTCOpMode;
@@ -23,45 +22,47 @@ import dev.nextftc.hardware.driving.HolonomicMode;
 import dev.nextftc.hardware.driving.MecanumDriverControlled;
 import dev.nextftc.hardware.impl.Direction;
 import dev.nextftc.hardware.impl.IMUEx;
+import dev.nextftc.hardware.impl.MotorEx;
 
 @TeleOp
 public class nextftc_test extends NextFTCOpMode {
 
+
+
     DriverControlledCommand driverControlled;
-    private  MotorEx frontLeftMotor = new MotorEx("front_left").reverse();
-    private  MotorEx frontRightMotor = new MotorEx("front_right");
-    private  MotorEx backLeftMotor = new MotorEx("back_left").reverse();
-    private  MotorEx backRightMotor = new MotorEx("back_right");
+
+    //Left motors r reversed, may be different tho on robot tho
+
+    private MotorEx frontLeft = new MotorEx("front_left").brakeMode().reversed();
+    private MotorEx frontRight = new MotorEx("front_right").brakeMode();
+    private MotorEx backLeft = new MotorEx("back_left").brakeMode().reversed();
+    private MotorEx backRight = new MotorEx("back_right").brakeMode();
+
+
+
+
+    //gyroscope
     private IMUEx imu = new IMUEx("imu", Direction.UP, Direction.FORWARD).zeroed();
-    private HolonomicMode mode;
 
     @Override
     public void onInit(){
         telemetry.addData("Status", "Initialized");
-
-        frontLeftMotor.setPower(DcMotor.ZeroPowerBehavior.BRAKE.ordinal());
-        frontRightMotor.setPower(DcMotor.ZeroPowerBehavior.BRAKE.ordinal());
-        backLeftMotor.setPower(DcMotor.ZeroPowerBehavior.BRAKE.ordinal());
-        backRightMotor.setPower(DcMotor.ZeroPowerBehavior.BRAKE.ordinal());
-
-
-
-
     }
-
     @Override
     public void onStartButtonPressed(){
-        DriverControlledCommand driverControlled = new MecanumDriverControlled(frontLeftMotor,
-                frontRightMotor,
-                backLeftMotor,
-                backRightMotor,
+        telemetry.addData("Status", "Started");
+        telemetry.update();
+        DriverControlledCommand driverControlled = new MecanumDriverControlled(frontLeft,
+                frontRight,
+                backLeft,
+                backRight,
                 Gamepads.gamepad1().leftStickY(),
                 Gamepads.gamepad1().leftStickX(),
                 Gamepads.gamepad1().rightStickX(),
                 new FieldCentric(imu)
-
-
         );
+
+
         driverControlled.schedule();
     }
 
