6f2c5661f93dfbb0ff9837b023ca237177cff0e7
oldmankuba <oldmankuba@gmail.com>
2025-11-11 15:27:27 -0600

    Fixed sequential group placement in respect to follower calls. Fixed Pose headings

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathing/autoOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pathing/autoOp.java
@@ -57,40 +57,69 @@ public class autoOp extends NextFTCOpMode {
     private final Pose shootPoseCloseRed = new Pose(84, 84, Math.toRadians(45)); //CHANGE ONCE VELOCITY DEFINED
 
     //object poses top to bottom
-    private final Pose objects1 = new Pose(96, 84, Math.toRadians(0));
-    private final Pose endpickup1 = new Pose(115, 84, Math.toRadians(0));
-    private final Pose objects2 = new Pose(96, 60, Math.toRadians(0));
-    private final Pose endpickup2 = new Pose(115, 60, Math.toRadians(0));
-    private final Pose objects3 = new Pose(96, 36, Math.toRadians(0));
-    private final Pose endpickup3 = new Pose(115, 36, Math.toRadians(0));
+    private final Pose objects1 = new Pose(96, 84, Math.toRadians(180));
+    private final Pose endpickup1 = new Pose(115, 84, Math.toRadians(180));
+    private final Pose objects2 = new Pose(96, 60, Math.toRadians(180));
+    private final Pose endpickup2 = new Pose(115, 60, Math.toRadians(180));
+    private final Pose objects3 = new Pose(96, 36, Math.toRadians(180));
+    private final Pose endpickup3 = new Pose(115, 36, Math.toRadians(180));
 
     private PathChain moveToShoot, moveToObjects1, pickupObjects1, moveToShoot1, moveToObjects2, pickupObjects2, moveToShoot2, moveToObjects3, pickupObjects3, moveToShoot3;
 
 
     public void buildPaths() {
 
-        moveToShoot = follower.pathBuilder().addPath(new BezierLine(startRedFar, shootPoseFarRed)).setLinearHeadingInterpolation(startRedFar.getHeading(), shootPoseFarRed.getHeading()).build();
+        moveToShoot = follower.pathBuilder()
+                .addPath(new BezierLine(startRedFar, shootPoseFarRed))
+                .setLinearHeadingInterpolation(startRedFar.getHeading(), shootPoseFarRed.getHeading())
+                .build();
 
-        moveToObjects1 = follower.pathBuilder().addPath(new BezierLine(shootPoseFarRed, objects1)).setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), objects1.getHeading()).build();
+        moveToObjects1 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFarRed, objects1))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), objects1.getHeading())
+                .build();
 
-        pickupObjects1 = follower.pathBuilder().addPath(new BezierLine(objects1, endpickup1)).setConstantHeadingInterpolation(Math.toRadians(0)).build();
+        pickupObjects1 = follower.pathBuilder()
+                .addPath(new BezierLine(objects1, endpickup1))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
 
-        moveToShoot1 = follower.pathBuilder().addPath(new BezierLine(endpickup1, shootPoseFarRed)).setLinearHeadingInterpolation(endpickup1.getHeading(), shootPoseFarRed.getHeading()).build();
+        moveToShoot1 = follower.pathBuilder()
+                .addPath(new BezierLine(endpickup1, shootPoseFarRed))
+                .setLinearHeadingInterpolation(endpickup1.getHeading(), shootPoseFarRed.getHeading())
+                .build();
 
-        moveToObjects2 = follower.pathBuilder().addPath(new BezierLine(shootPoseFarRed, objects2)).setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), objects2.getHeading()).build();
+        moveToObjects2 = follower.
+                pathBuilder().addPath(new BezierLine(shootPoseFarRed, objects2))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), objects2.getHeading())
+                .build();
 
-        pickupObjects2 = follower.pathBuilder().addPath(new BezierLine(objects2, endpickup2)).setConstantHeadingInterpolation(Math.toRadians(0)).build();
+        pickupObjects2 = follower.pathBuilder()
+                .addPath(new BezierLine(objects2, endpickup2))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
 
-        moveToShoot2 = follower.pathBuilder().addPath(new BezierLine(endpickup2, shootPoseFarRed)).setLinearHeadingInterpolation(endpickup2.getHeading(), shootPoseFarRed.getHeading()).build();
+        moveToShoot2 = follower.pathBuilder()
+                .addPath(new BezierLine(endpickup2, shootPoseFarRed))
+                .setLinearHeadingInterpolation(endpickup2.getHeading(), shootPoseFarRed.getHeading())
+                .build();
 
-        moveToObjects3 = follower.pathBuilder().addPath(new BezierLine(shootPoseFarRed, objects3)).setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), objects3.getHeading()).build();
+        moveToObjects3 = follower.pathBuilder()
+                .addPath(new BezierLine(shootPoseFarRed, objects3))
+                .setLinearHeadingInterpolation(shootPoseFarRed.getHeading(), objects3.getHeading())
+                .build();
 
-        pickupObjects3 = follower.pathBuilder().addPath(new BezierLine(objects3, endpickup3)).setConstantHeadingInterpolation(Math.toRadians(0)).build();
+        pickupObjects3 = follower.pathBuilder()
+                .addPath(new BezierLine(objects3, endpickup3))
+                .setConstantHeadingInterpolation(Math.toRadians(180))
+                .build();
 
-        moveToShoot3 = follower.pathBuilder().addPath(new BezierLine(endpickup3, shootPoseFarRed)).setLinearHeadingInterpolation(endpickup3.getHeading(), shootPoseFarRed.getHeading()).build();
+        moveToShoot3 = follower.pathBuilder()
+                .addPath(new BezierLine(endpickup3, shootPoseFarRed))
+                .setLinearHeadingInterpolation(endpickup3.getHeading(), shootPoseFarRed.getHeading())
+                .build();
 
     }
-
     public void autonomousPathUpdate() {
         switch (pathState) {
             case 0:
@@ -98,6 +127,7 @@ public class autoOp extends NextFTCOpMode {
                 //flywheel shoot
                 new Delay(1);
 
+                follower.followPath(moveToShoot, true);
                 //for shooting 2 balls
                 new SequentialGroup(
                         //flywheel shoot,
@@ -110,26 +140,26 @@ public class autoOp extends NextFTCOpMode {
                         Loader.INSTANCE.reset,
                         //Shoot second preload
                         Flywheel.INSTANCE.shoot, Flywheel.INSTANCE.stop);
-                follower.followPath(moveToShoot, true);
                 setPathState(1);
                 break;
             case 1:
                 if (!follower.isBusy()) {
+                    follower.followPath(moveToObjects1, true);
                     //start intake
                     Intake.INSTANCE.spin();
-                    follower.followPath(moveToObjects1, true);
                     setPathState(2);
                 }
                 break;
             case 2:
                 if (!follower.isBusy()) {
+                    follower.followPath(pickupObjects1, true);
                     //stop intake
                     Intake.INSTANCE.stop();
-                    follower.followPath(pickupObjects1, true);
                     setPathState(3);
                 }
             case 3:
                 if (!follower.isBusy()) {
+                    follower.followPath(moveToShoot1, true);
                     //pause
                     new Delay(3);
                     //flywheel shoot
@@ -144,16 +174,15 @@ public class autoOp extends NextFTCOpMode {
                             Loader.INSTANCE.reset,
                             //Shoot second preload
                             Flywheel.INSTANCE.shoot, Flywheel.INSTANCE.stop);
-                    follower.followPath(moveToShoot1, true);
                     setPathState(4);
                 }
                 break;
             case 4:
                 if (!follower.isBusy()) {
+
+                    follower.followPath(moveToObjects2, true);
                     Intake.INSTANCE.spin();
                     //start intake
-                    follower.followPath(moveToObjects2, true);
-
                     setPathState(5);
                 }
                 break;
@@ -168,6 +197,7 @@ public class autoOp extends NextFTCOpMode {
             case 6:
                 if (!follower.isBusy()) {
                     //pause
+                    follower.followPath(moveToShoot2, true);
                     new Delay(.5);
                     //flywheel shoot
                     new SequentialGroup(
@@ -183,15 +213,14 @@ public class autoOp extends NextFTCOpMode {
                             Flywheel.INSTANCE.shoot,
                             Flywheel.INSTANCE.stop
                     );
-                    follower.followPath(moveToShoot2, true);
                     setPathState(7);
                 }
                 break;
             case 7:
                 if (!follower.isBusy()) {
+                    follower.followPath(moveToObjects3, true);
                     //start intake
                     Intake.INSTANCE.spin();
-                    follower.followPath(moveToObjects3, true);
                     setPathState(8);
                 }
                 break;
@@ -206,6 +235,7 @@ public class autoOp extends NextFTCOpMode {
             case 9:
                 if (!follower.isBusy()) {
                     //pause
+                    follower.followPath(moveToShoot3, true);
                     new Delay(.5);
                     //flywheel shoot
                     new SequentialGroup(
@@ -221,7 +251,6 @@ public class autoOp extends NextFTCOpMode {
                             Flywheel.INSTANCE.shoot,
                             Flywheel.INSTANCE.stop
                     );
-                    follower.followPath(moveToShoot3, true);
                     setPathState(10);
                 }
                 break;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -8,26 +8,23 @@ import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
 import com.pedropathing.ftc.localization.constants.ThreeWheelConstants;
-import com.pedropathing.ftc.localization.constants.ThreeWheelIMUConstants;
 import com.pedropathing.paths.PathConstraints;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(11.5)
-            .forwardZeroPowerAcceleration(-53.686793658117935)
-            .lateralZeroPowerAcceleration(-71.77801)
+            .mass(13)
+            .forwardZeroPowerAcceleration(-41.278)
+            .lateralZeroPowerAcceleration(-59.7819)
             .useSecondaryTranslationalPIDF(false)
             .useSecondaryHeadingPIDF(false)
             .useSecondaryDrivePIDF(false)
             .centripetalScaling(0.0005)
-            .translationalPIDFCoefficients(new com.pedropathing.control.PIDFCoefficients(0.12, 0, 0.01, 0.02))
-            .headingPIDFCoefficients(new PIDFCoefficients(1.6, 0, 0.1, .02))
+            .translationalPIDFCoefficients(new com.pedropathing.control.PIDFCoefficients(0.1, 0, 0.01, 0))
+            .headingPIDFCoefficients(new PIDFCoefficients(2, 0, 0.1, 0))
             .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.1, 0, 0, 0.6, 0));
-
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .leftFrontMotorName("leftFront")
@@ -36,28 +33,25 @@ public class Constants {
             .rightRearMotorName("rightRear")
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(52.41567608362866)
-            .yVelocity(30.88);
+            .xVelocity(57.8741)
+            .yVelocity(52.295);
 
-    public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-            .forwardTicksToInches(0.002980426565529629)
-            .strafeTicksToInches(0.002980426565529629)
-            .turnTicksToInches(0.00208970)
-            .leftPodY(7)
-            .rightPodY(-7)
-            .strafePodX(-6.5)
-            .leftEncoder_HardwareMapName("leftFront")
-            .rightEncoder_HardwareMapName("rightFront")
-            .strafeEncoder_HardwareMapName("rightRear")
-            .leftEncoderDirection(Encoder.REVERSE)
-            .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.REVERSE)
-            .IMU_HardwareMapName("imu")
-            //the intake is back
-            .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                    RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+    public static ThreeWheelConstants localizerConstants =
+            new ThreeWheelConstants()
+                    .forwardTicksToInches(.001989436789)
+                    .strafeTicksToInches(.001989436789)
+                    .turnTicksToInches(.001989436789)
+                    .leftPodY(1)
+                    .rightPodY(-1)
+                    .strafePodX(-2.5)
+                    .leftEncoder_HardwareMapName("leftFront")
+                    .rightEncoder_HardwareMapName("rightRear")
+                    .strafeEncoder_HardwareMapName("rightFront")
+                    .leftEncoderDirection(Encoder.REVERSE)
+                    .rightEncoderDirection(Encoder.REVERSE)
+                    .strafeEncoderDirection(Encoder.FORWARD);
 
     public static PathConstraints pathConstraints = new PathConstraints(
             0.995,
@@ -69,7 +63,7 @@ public class Constants {
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .mecanumDrivetrain(driveConstants)
-                .threeWheelIMULocalizer(localizerConstants)
+                .threeWheelLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
                 .build();
     }
